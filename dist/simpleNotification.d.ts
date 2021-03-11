750b3c1274c367ece2c58c97a0905e5d328e8362
Glagan <Glagan@users.noreply.github.com>
2021-03-12 00:41:57 +0100

    Update SimpleNotification definition

--- dist/simpleNotification.d.ts
+++ dist/simpleNotification.d.ts
@@ -1,6 +1,16 @@
 declare module 'SimpleNotification' {
-	export type NotificationPosition = 'top-left' | 'top-right' | 'bottom-left' | 'bottom-center' | 'bottom-right';
+	export type Position = 'top-left' | 'top-right' | 'bottom-left' | 'bottom-center' | 'bottom-right';
 	export type Type = 'success' | 'info' | 'error' | 'warning' | 'message';
+	export type InsertAnimation =
+		| 'default-insert'
+		| 'insert-left'
+		| 'insert-right'
+		| 'insert-top'
+		| 'insert-bottom'
+		| 'fadein'
+		| 'scalein'
+		| 'rotatein';
+	export type RemoveAnimation = 'fadeout' | 'scaleout' | 'rotateout';
 	export type eventCallback = (notification: SimpleNotification) => void;
 	export type Events = {
 		onCreate?: eventCallback;
@@ -21,8 +31,17 @@ declare module 'SimpleNotification' {
 		buttons?: Button[];
 	}
 
+	export interface TagDescription {
+		type: string;
+		class: string;
+		open: string;
+		close: string;
+		attributes: { textContent: string | boolean } & { [key: string]: string };
+		textContent: string;
+	}
+
 	export interface Options {
-		position: NotificationPosition;
+		position: Position;
 		maxNotifications: number;
 		removeAllOnDisplay: boolean;
 		pauseOnHover: boolean;
@@ -32,24 +51,16 @@ declare module 'SimpleNotification' {
 		sticky: boolean;
 		events: Partial<Events>;
 		insertAnimation: {
-			name:
-				| 'default-insert'
-				| 'insert-left'
-				| 'insert-right'
-				| 'insert-top'
-				| 'insert-bottom'
-				| 'fadein'
-				| 'scalein'
-				| 'rotatein';
-			duration: number;
+			name: InsertAnimation;
+			duration?: number;
 		};
 		removeAnimation: {
-			name: 'fadeout' | 'scaleout' | 'rotateout';
-			duration: number;
+			name: RemoveAnimation;
+			duration?: number;
 		};
 	}
 
-	declare global {
+	global {
 		class SimpleNotification {
 			constructor(options?: Partial<Options>);
 			options: Options;
@@ -66,8 +77,8 @@ declare module 'SimpleNotification' {
 			removeExtinguish: () => void;
 			// SimpleNotification
 			make(classes: string[]): void;
-			setTitle(type: Type): void;
-			setType(title: string): void;
+			setTitle(type: string): void;
+			setType(title: Type): void;
 			setImage(src: string): void;
 			setText(content: string): void;
 			addCloseButton(): void;
@@ -77,39 +88,31 @@ declare module 'SimpleNotification' {
 			addProgressBar(): void;
 			display(): void;
 			remove(): void;
-			close(fromUser: boolean = false): void;
+			close(fromUser?: boolean): void;
 			closeAnimated(): void;
 			addExtinguishFct(): void;
 			removeExtinguishFct(): void;
 			disableButtons(): void;
-			static wrappers: Partial<Record<NotificationPosition, HTMLElement>>;
+			static wrappers: Partial<Record<Position, HTMLElement>>;
 			static displayed: SimpleNotification[];
 			static _options: Options;
 			static makeWrapper(position: number): void;
-			static create(
-				classes: string[],
-				content: Partial<Content>,
-				options: Partial<Options> = {}
-			): SimpleNotification;
-			static success(content: Partial<Content>, options: Partial<Options> = {}): SimpleNotification;
-			static info(content: Partial<Content>, options: Partial<Options> = {}): SimpleNotification;
-			static error(content: Partial<Content>, options: Partial<Options> = {}): SimpleNotification;
-			static warning(content: Partial<Content>, options: Partial<Options> = {}): SimpleNotification;
-			static message(content: Partial<Content>, options: Partial<Options> = {}): SimpleNotification;
-			static custom(
-				classes: string[],
-				content: Partial<Content>,
-				options: Partial<Options> = {}
-			): SimpleNotification;
+			static create(classes: string[], content: Partial<Content>, options?: Partial<Options>): SimpleNotification;
+			static success(content: Partial<Content>, options?: Partial<Options>): SimpleNotification;
+			static info(content: Partial<Content>, options?: Partial<Options>): SimpleNotification;
+			static error(content: Partial<Content>, options?: Partial<Options>): SimpleNotification;
+			static warning(content: Partial<Content>, options?: Partial<Options>): SimpleNotification;
+			static message(content: Partial<Content>, options?: Partial<Options>): SimpleNotification;
+			static custom(classes: string[], content: Partial<Content>, options?: Partial<Options>): SimpleNotification;
 			static addTag(name: string, tag: TagDescription): void;
-			static deepAssign(target: Object, ...objs): Object;
+			static deepAssign(target: Object, ...objs: any[]): Object;
 			static options(options: Partial<Options>): void;
 			// SimpleMark
 			static refreshTokens?: boolean;
 			static tags: Record<string, TagDescription>;
 			static textToNode(text: string, node: HTMLElement): HTMLElement;
 			static buildNode(string: string, node: HTMLElement): HTMLElement;
-			static firstUnbreakChar(text: string, char: string, start: number = 0): number;
+			static firstUnbreakChar(text: string, char: string, start?: number): number;
 			static searchToken(string: string, token: string, start: number): [number, number];
 			static breakString(string: string, tag: TagDescription, start: number, end: number): [number, number];
 			static joinString(arr: string[]): string;
